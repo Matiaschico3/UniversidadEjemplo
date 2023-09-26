6588ca992b17feaed6453a555cedfdd00516222c
Matiaschico3 <matiaschico3@gmail.com>
2023-09-26 11:33:41 -0300

    ahora si esta bien
    xd

--- src/universidadejemplo/Vistas/Materias.java
+++ src/universidadejemplo/Vistas/Materias.java
@@ -330,7 +330,7 @@ public class Materias extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_jbNuevoActionPerformed
 
     private void jtNombreKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtNombreKeyReleased
-       if(!jtNombre.getText().isEmpty() && !jtAnio.getText().isEmpty() && !jtCodigo.getText().isEmpty()){
+       if(!jtNombre.getText().isEmpty() && !jtAnio.getText().isEmpty()){
         habilitarBotones();
        }else{
              desHabilitarBotones();
@@ -338,7 +338,7 @@ public class Materias extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_jtNombreKeyReleased
 
     private void jtAnioKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtAnioKeyReleased
-       if(!jtNombre.getText().isEmpty() && !jtAnio.getText().isEmpty() && !jtCodigo.getText().isEmpty()){
+       if(!jtNombre.getText().isEmpty() && !jtAnio.getText().isEmpty()){
         habilitarBotones();
        }else{
              desHabilitarBotones();
